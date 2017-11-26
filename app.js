0bae9bbef7b7151fd4717ec6764f27d4f5d51bf2
Sulaiman Sanusi <sulaiman.sanusi@icloud.com>
2017-11-26 22:02:28 +0000

    Done.

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ var express = require('express')
 var app = express()
 
 // Build Your Route Here
+app.get('/', function(req, resp){
+  resp.send(`Hello, World!`)
+})
+
+app.listen(3000);
 
 
 module.exports = app
